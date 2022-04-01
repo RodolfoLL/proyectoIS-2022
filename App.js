6c2c46251979bb00a37c5d4e1d3ec0cfbb2e5f76
RodolfoLL <201606416@est.umss.edu>
2022-04-01 16:53:28 -0400

    firebase configurado

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
+import { doc, setDoc } from 'firebase/firestore';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Button } from 'react-native';
+import {db} from './database/firebase'
 export default function App() {
+
+  const Create = ()=>{
+
+    const myDoc = doc(db,'MyCollectionName','MyDocument');
+    const docdata = {
+      'name':"lozano",
+      'edad': "12"
+    }
+    setDoc(myDoc,docdata)
+      .then(()=> {
+        alert('document created');
+      })
+      .catch((error)=>{
+       alert(error.mesagge)
+    })
+
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title = 'create New' onPress={Create}></Button>
       <StatusBar style="auto" />
     </View>
   );
