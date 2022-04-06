70f690f5cf34bf09f980e335b98a350745b87aeb
Luisa Mora <luisamora895@gmail.com>
2022-04-06 00:43:08 -0400

    Pantalla1 interfaz casi terminada

--- App.js
+++ App.js
@@ -1,41 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { doc, setDoc } from 'firebase/firestore';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native';
-import {db} from './database/firebase'
-export default function App() {
-
-  const Create = ()=>{
-
-    const myDoc = doc(db,'MyCollectionName','MyDocument');
-    const docdata = {
-      'name':"lopez",
-      'edad': "12"
-    }
-    setDoc(myDoc,docdata)
-      .then(()=> {
-        alert('document created');
-      })
-      .catch((error)=>{
-       alert(error.mesagge)
-    })
-
-  }
-
-
-  return (
-    <View style={styles.container}>
-      <Button title = 'create New' onPress={Create}></Button>
-      <StatusBar style="auto" />
-    </View>
+import React from "react";
+import {Text,View,StyleSheet,Button,TextInput} from 'react-native';
+import { CheckBox } from 'react-native-elements';
+const App = () => {
+  return (<View style={styles.container}>
+  <Text style={styles.title}>Nombre del Medicamento</Text>
+  <TextInput style={styles.inputS}
+    placeholder="nombreMed"
+  />
+  <Text style={styles.title}>Tipo de administracion</Text>
+  <View style={styles.opciones}>
+   <CheckBox
+  title='Via Oral'
+/>
+  <CheckBox
+  title='Via Intramuscular'
+/>
+ <CheckBox
+  title='Via Inalatoria'
+/>
+  <CheckBox
+  title='Via Nasal'
+/>
+ <CheckBox
+  title='Via Topica'
+/>
+  <CheckBox
+  title='Via Oftalmologica'
+/>
+ <CheckBox
+  title='Via parental'
+/>
+</View>
+    <Button
+    title="Continuar"
+    color="#0093B7"
+    onPress={()=>console.log('continuar')}
+  />
+  </View>
   );
-}
-
+};
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{flex:1,
+             backgroundColor:'#001B48',
+             padding:30,
+             paddingTop:70,
+            },
+  title:{fontSize:30,
+         color:'white'
+         },
+  imagen:{
+    height:200,
+    width:200,
+    borderRadius:100
   },
+  inputS:{
+    height:40,
+    width:350,
+    borderColor:'white',
+    borderWidth:3,
+    borderRadius:5,
+    marginTop:30,
+    marginBottom:30,
+    padding:15,
+  },
+  subtitle:{
+  color:'white',
+  margin:10,
+  fontSize:25
+  },
+  opciones:{
+  margin:20
+  }
 });
+export default App;
