83bc3c95bc6558843206b6a5adaaa6e55ba53dbe
Maxjhon5 <jhonsor55@gmail.com>
2022-04-07 16:16:15 -0400

    orden de carpetas y tab Navigation

--- App.js
+++ App.js
@@ -1,32 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-//import { doc, setDoc } from 'firebase/firestore';
-import { StyleSheet, Text, View, Button } from 'react-native';
-//import {db} from './database/firebase'
-import {NavigationContainer} from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import DuracionTratamiento from './screens/DuracionTratamiento'
-import FechaFinal from './screens/FechaFinal'
-
-const Stack = createNativeStackNavigator()
+import React from 'react';
+import Navigation from './navigation';
+// import { doc, setDoc } from 'firebase/firestore';
+// import {db} from './database/firebase'
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        
-        <Stack.Screen name="DuracionTratamiento" component={DuracionTratamiento}/>
-        <Stack.Screen name="FechaFinal" component={FechaFinal}/>
-        
-      </Stack.Navigator>
-    </NavigationContainer>
+  return(
+    <Navigation/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ navigation.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons'; 
+//Pantallas que utilize de ejemplo para la navegacion
+import DuracionTratamiento from './screens/DuracionTratamiento'
+import FechaFinal from './screens/FechaFinal'
+
+const HomeStackNavigator = createNativeStackNavigator();
+
+function MyStacks(){
+    return(
+        <HomeStackNavigator.Navigator initialRouteName='Medicate'>
+            <HomeStackNavigator.Screen name="DuracionTratamiento" component={DuracionTratamiento}/>
+            <HomeStackNavigator.Screen name="FechaFinal" component={FechaFinal}/>
+        </HomeStackNavigator.Navigator>
+    );
+}
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs(){
+    return(
+        <Tab.Navigator
+            initialRouteName='Medicate'
+            screenOptions={{
+                tabBarActiveTintColor:'black',
+                tabBarStyle:{
+                    backgroundColor:'#0093B7',
+                }
+            }}
+        >
+            <Tab.Screen 
+                name="Medicate" 
+                component={MyStacks}
+                options={{
+                    tabBarLabel:'inicio',
+                    tabBarIcon:({color,size})=>(
+                        <Ionicons name="home-outline" size={size} color={color} />
+                    ),
+                    headerShown:false,
+                }}
+            />
+        </Tab.Navigator>
+    );
+}
+export default function Navigation(){
+    return(
+        <NavigationContainer>
+            <MyTabs />
+        </NavigationContainer>
+    );
+}
