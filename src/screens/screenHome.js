ff33717f0c348dfd40c63e34fe0f66fbfd08c109
jhosmanB <102555068+jhosmanB@users.noreply.github.com>
2022-04-10 13:56:15 -0400

    copia sreen home

--- src/screens/screenHome.js
+++ src/screens/screenHome.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, TouchableOpacity, ScrollView, Image,Alert } from "react-native";
-import image from '../../assets/medicate.png'
+import { View, Text, TouchableOpacity, ScrollView, Image} from "react-native";
+import image from '../assets/medicate.png'
 import {db} from '../../database/firebase'
-
-import { collection, query, where, getDocs, deleteDoc,doc } from "firebase/firestore";
+import { StatusBar } from 'expo-status-bar';
+import { collection, query, where, getDocs } from "firebase/firestore";
 import { FlatList } from "react-native-gesture-handler";
 import { render } from "react-dom";
-import { ListItem, Avatar } from 'react-native-elements'
-import { async } from "@firebase/util";
+import { ListItem ,Icon} from 'react-native-elements';
+
 
 const screenHome = ({ navigation }) => {
 
@@ -35,103 +35,97 @@ const screenHome = ({ navigation }) => {
 
         setRecordatorios(listaRecordatorios);
     }
-    console.log(recordatorios)
+
     useEffect(() => {
             listarRecordatorio()
     }, []);
-    const elimnarRecordatorio = async (id) =>{
-        const docRef = doc(db,"Recordatorios",id)
-        
-        await deleteDoc(docRef)
-        navigation.navigate("screenHome")
-    }
-    const confirmarElimniar = (id) => {
-        Alert.alert("Eliminar recordatorio", "estas seguro?",[
-        {text: "Si" ,onPress: () =>{ elimnarRecordatorio(id)} },
-        {text: "No" ,onPress: () =>{ console.log("ok sin elimnar")} }
-        ])
-
-    }
+   
     return (
+      
+        <View>
         <ScrollView style={{ backgroundColor: '#001B48' }}>
             <View style={{
                 fontSize: 30,
                 alignItems: "center",
-                marginTop: "10%"
+                marginTop: "5%"
+                
             }}>
-                <Text style={{ fontSize: 50, color: 'white' }}>
-                    MEDICATE
+                <Text style={{ fontSize: 50, color: 'white', fontWeight: 'bold' }}>
+                    MEDICATE 
                 </Text>
-                <View style={{width: "90%", height: "40%"}}>
+                <View style={{width: "90%", height: "30%"}}>
                     {recordatorios.map((recordatorio) => {
                         return (
-                          <ListItem key={recordatorio.id} bottomDivider>
-                            <ListItem.Content bottomDivider style={{ backgroundColor: "#001B48",width: "100%", height: "100%"}}>
-                              <ListItem.Title style={{ color: "white"}}>{recordatorio.nombreMed}</ListItem.Title>
-
-                              <ListItem.Subtitle style={{ color: "white"}}>Tipo de administracion: {recordatorio.tipoAdm}</ListItem.Subtitle>
+                          <ListItem key={recordatorio.id} style={{marginBottom: 10}}>
+                            
+                            <ListItem.Content bottomDivider style={{width: "100%", height: "100%",}}>
+                             
+                              <ListItem.Title style={{ color: "black", fontSize: 25, fontWeight: "bold"}}>{recordatorio.nombreMed}</ListItem.Title>
                               
-                              <ListItem.Subtitle style={{ color: "white"}}>Hora: {recordatorio.hora}</ListItem.Subtitle>
-                              <ListItem.Subtitle style={{ color: "white"}}>Duracion hasta: {recordatorio.duracion}</ListItem.Subtitle>
-                              <TouchableOpacity
-                                    onPress={() => navigation.navigate("Editar Medicamento",{
-                                        id: recordatorio.id,
-                                        nombreMed:recordatorio.nombreMed,
-                                        tipoAdm: recordatorio.tipoAdm,
-                                        dose: recordatorio.dose,
-                                        quantity: recordatorio.quantity,
-                                        item: recordatorio.item,
-                                        hora: recordatorio.hora,
-                                        duracion: recordatorio.duracion,
-                
-                                    })}
-                                    style={{
-                                        backgroundColor: "#0093B7",
-                                    
-                                    }}
-                                >
-                                    <Text> EDIT</Text>
-                                </TouchableOpacity>
-                                <TouchableOpacity
-                                        onPress={() => confirmarElimniar(recordatorio.id)}
-                                        style={{
-                                            backgroundColor: "#0093B7",
-                                        
-                                        }}
-                                    >
-                                        <Text> eliminar</Text>
-                                    </TouchableOpacity>
+                              <ListItem.Subtitle style={{ color: "black"}}>Tipo de administracion: {recordatorio.tipoAdm}</ListItem.Subtitle>
+                              <ListItem.Subtitle style={{ color: "black"}}>Dosis: {recordatorio.dose}</ListItem.Subtitle>
+                              <ListItem.Subtitle style={{ color: "black"}}>Cantidad de medicamentos: {recordatorio.quantity}</ListItem.Subtitle>
+                              <ListItem.Subtitle style={{ color: "black"}}>Frecuencia: {recordatorio.item}</ListItem.Subtitle>
+                              <ListItem.Subtitle style={{ color: "black"}}>Hora: {recordatorio.hora}</ListItem.Subtitle>
+                              <ListItem.Subtitle style={{ color: "black"}}>Duracion hasta: {recordatorio.duracion}</ListItem.Subtitle>
+
+                              
                             </ListItem.Content>
+                            
+                            <View style={{ flexDirection: "column", height: "100%"}}>
+                                <Icon type="material-community" 
+                                    name={"pencil-circle"} size={50} 
+                                    color={"#0093B7"} 
+                                    onPress={() => navigation.navigate("Registro de Medicamento")}  
+                                    style={{ marginTop: "0%"}}/>
+                                <Icon type="material-community" 
+                                    name={"delete-circle"} 
+                                    size={50} color={"#0093B7"} 
+                                    onPress={() => navigation.navigate("Registro de Medicamento")} 
+                                    style={{ marginTop: "80%"}}/>
+                            </View>
                           </ListItem>
                         );
                       })}
                 </View>
                 
                       
-                <TouchableOpacity
+                
+            </View>
+
+        </ScrollView>
+        <TouchableOpacity
                     onPress={() => navigation.navigate("Registro de Medicamento")}
                     style={{
-                        backgroundColor: "#0093B7",
-                        width: 150,
-                        height: 70,
-                        borderRadius: 5,
+                        backgroundColor: "#001B48",
+                        width: 80,
+                        height: 40,
+                        borderRadius: 1000,
                         justifyContent: "center",
-                        marginTop: "25%"
+                        marginTop: "15%",
+                        position: 'absolute',
+                        bottom: 0,
+                        left:260,
+                        top:460 
                     }}
                 >
                     <Text
                         style={{
-                            fontSize: 20,
+                            fontSize: 40,
                             textAlign: "center",
                             color: "white",
-                            width: 150
+                            width: 80,
+                            fontWeight: "bold"
                         }}
-                    >Añadir recordatorio</Text>
+                    >+</Text>
                 </TouchableOpacity>
-            </View>
-
-        </ScrollView>
+                <StatusBar style="auto" />
+         
+                       
+    </View>
+   
     );
+                                    
 }
 
 export default screenHome;
