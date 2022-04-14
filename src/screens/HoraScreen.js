c87f2802f064710254386d4cccb9eb9bbc8d9930
RodolfoLL <201606416@est.umss.edu>
2022-04-14 16:54:34 -0400

    agregando AM-PM a la seleccion de horas

--- src/screens/HoraScreen.js
+++ src/screens/HoraScreen.js
@@ -48,7 +48,18 @@ const HoraScreen=(props)=>{
                 setShow(false);
                 const currentDate = selectedDate || date;
                 setDate(currentDate);
-                let template = new Date(currentDate).toTimeString().substring(0,5);
+                let template = new Date(currentDate);
+                const  formatAMPM = (date)=> {
+                    var hours = date.getHours();
+                    var minutes = date.getMinutes();
+                    var ampm = hours >= 12 ? 'PM' : 'AM';
+                    hours = hours % 12;
+                    hours = hours ? hours : 12; // the hour '0' should be '12'
+                    minutes = minutes < 10 ? '0'+minutes : minutes;
+                    var strTime = hours + ':' + minutes + ' ' + ampm;
+                    return strTime;
+                  }
+                  template = formatAMPM(template);
                 setdatos([...datos,template]);
                 setcontador(contador -1);
             }else{
