dcf44a1613363ca2f07227036c6a234cb1d5d18b
Luisa Mora <luisamora895@gmail.com>
2022-04-29 12:58:22 -0400

    Notificacion en Pantalla de Inicio pruebas

--- src/screens/CantidadMedicamentos.js
+++ src/screens/CantidadMedicamentos.js
@@ -1,21 +1,8 @@
-
-import Constants from 'expo-constants';
-import * as Notifications from 'expo-notifications';
 import React, { useState, useEffect, useRef } from 'react';
 import { Button, View, ScrollView, Text,TouchableOpacity } from "react-native";
 import { Picker } from '@react-native-picker/picker'
 import { StyleSheet } from "react-native";
 
-//Notificaciones 
-Notifications.setNotificationHandler({
-    handleNotification: async () => ({
-      shouldShowAlert: true,
-      shouldPlaySound: false,
-      shouldSetBadge: false,
-    }),
-  });
-
-//Visualizacion 
 function generarArregloDosis(name) {
     let arreglo = []
     for (let i = 1; i <= 10; i++) {
@@ -38,28 +25,6 @@ function generarArregloDosis(name) {
     return arreglo;
 }
 const CantidadMedicamentos = ({ route, navigation }) => {
-  //Constantes de los medicamentos 
-  const [expoPushToken, setExpoPushToken] = useState('');
-  const [notification, setNotification] = useState(false);
-  const notificationListener = useRef();
-  const responseListener = useRef();
-
-  useEffect(() => {
-    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
-
-    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
-      setNotification(notification);
-    });
-
-    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-      console.log(response);
-    });
-
-    return () => {
-      Notifications.removeNotificationSubscription(notificationListener.current);
-      Notifications.removeNotificationSubscription(responseListener.current);
-    };
-  }, []);
 
     let tipoDosis = ""
     const [selectDose, setselectDose] = useState("1");
@@ -173,36 +138,6 @@ async function schedulePushNotification() {
       trigger: { seconds: 2 },
     });
   }
-async function registerForPushNotificationsAsync() {
-  let token;
-  if (Constants.isDevice) {
-    const { status: existingStatus } = await Notifications.getPermissionsAsync();
-    let finalStatus = existingStatus;
-    if (existingStatus !== 'granted') {
-      const { status } = await Notifications.requestPermissionsAsync();
-      finalStatus = status;
-    }
-    if (finalStatus !== 'granted') {
-      alert('Failed to get push token for push notification!');
-      return;
-    }
-    token = (await Notifications.getExpoPushTokenAsync()).data;
-    console.log(token);
-  } else {
-    alert('Must use physical device for Push Notifications');
-  }
-
-  if (Platform.OS === 'android') {
-    Notifications.setNotificationChannelAsync('default', {
-      name: 'default',
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: '#FF231F7C',
-    });
-  }
-
-  return token;
-}
 const STYLE_GROUP = StyleSheet.create(
     {
         containerMain:
