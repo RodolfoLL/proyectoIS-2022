23bff7bc1fae0f1d1fb2c3b07c2e917d5675d530
Gomer Claure <gomerclaureb@gmail.com>
2022-05-20 07:42:08 -0400

    Agregando un Alert de contra incorrecta y refactor

--- src/screens/verificarContraseña.js
+++ src/screens/verificarContraseña.js
@@ -1,92 +1,96 @@
 import { useState } from "react";
-import { View,StyleSheet,TouchableOpacity,ActivityIndicator,Dimensions,Platform,Alert,Image } from "react-native";
-import { Input,Button,Icon,Text,Overlay } from "react-native-elements";
-import {KeyboardAwareScrollView} from "react-native-keyboard-aware-scroll-view"
-import{useHeaderHeight } from "@react-navigation/elements"
+import { View, StyleSheet, TouchableOpacity, ActivityIndicator, Dimensions, Platform, Alert, Image } from "react-native";
+import { Input, Button, Icon, Text, Overlay } from "react-native-elements";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view"
+import { useHeaderHeight } from "@react-navigation/elements"
 import { size } from "lodash";
-import {getAuth,updateProfile,updateEmail,updatePassword, deleteUser, signInWithEmailAndPassword} from "firebase/auth"
-import {app} from '../../database/firebase'
-import { collection, query, where, getDocs ,doc, deleteDoc,updateDoc, onSnapshot} from "firebase/firestore"
-import {db} from '../../database/firebase'
-import { Ionicons } from '@expo/vector-icons'; 
+import { getAuth, updateProfile, updateEmail, updatePassword, deleteUser, signInWithEmailAndPassword } from "firebase/auth"
+import { app } from '../../database/firebase'
+import { collection, query, where, getDocs, doc, deleteDoc, updateDoc, onSnapshot } from "firebase/firestore"
+import { db } from '../../database/firebase'
+import { Ionicons } from '@expo/vector-icons';
 //import { deleteUser } from "firebase/auth";
 
-const verificarContraseña = ({navigation, route}) =>{
-    
-    const [mostarContra, setmostarContra] = useState(false)
-    const [password, setPassword] = useState("")
-    const [errorContra,seterrorContra]= useState("")
+const verificarContraseña = ({ navigation, route }) => {
 
-    const auth = getAuth();
-    const user = auth.currentUser;
-    const email = user.email;
-    const {Tipo} = route.params;
+  const [mostarContra, setmostarContra] = useState(false)
+  const [password, setPassword] = useState("")
+  const [errorContra, seterrorContra] = useState("")
 
-   
+  const auth = getAuth();
+  const user = auth.currentUser;
+  const email = user.email;
+  const { Tipo } = route.params;
 
-    let nombreBoton = "";
-     if(Tipo == "Actualizar"){
-       navigation.setOptions({title: 'Editar Datos del Usuario'})
-       nombreBoton = "Actualizar"
 
-     }else{
-       nombreBoton ="Eliminar"
-       navigation.setOptions({title: 'Eliminar Cuenta'})
-     }
-     
-   function validarContra(contra){
-        const regex = /^[0-9a-zA-Z\_]+$/
-        return regex.test(contra) 
+
+  let nombreBoton = "";
+  if (Tipo == "Actualizar") {
+    navigation.setOptions({ title: 'Editar Datos del Usuario' })
+    nombreBoton = "Actualizar"
+
+  } else {
+    nombreBoton = "Eliminar"
+    navigation.setOptions({ title: 'Eliminar Cuenta' })
+  }
+
+  function validarContra(contra) {
+    const regex = /^[0-9a-zA-Z\_]+$/
+    return regex.test(contra)
+  }
+  async function inicioSesion() {
+    let respuesta = { estado: true }
+    await signInWithEmailAndPassword(auth, email, password)
+      .then(credencial => { console.log("Se inició sesion") })
+      .catch(errorr => {
+        respuesta.estado = false
+      })
+    return respuesta;
+  }
+  const verificarContraseña = async () => {
+    seterrorContra("")
+    console.log(password)
+    if (!validarContra(password)) {
+      seterrorContra("La contraseña no debe tener caracteres especiales o espacios")
+    } else {
+      const resultInicioSesion = await inicioSesion()
+      const estaReautenticado = resultInicioSesion.estado
+      if (estaReautenticado) {
+        if (Tipo == "Eliminar") {
+          deleteUser(user)
+            .then(() => {
+              console.log(user)
+
+              navigation.navigate("Login");
+
+              Alert.alert("Cuenta Eliminada ", "La cuenta fue eliminada corrcetamnete", [
+                { text: "OK", onPress: () => { console.log("ok a Login") } }
+              ])
+            })
+            .catch(error => {
+              console.log(error)
+              const errorCode = error.code;
+              const errorMessage = error.message;
+
+            }
+            )
+        } else {
+          navigation.navigate("Administrar Cuenta");
+        }
+
+      }else{
+        Alert.alert("Error", "La contraseña ingresada es incorecta.", [
+          { text: "OK", onPress: () => { console.log("ok contraseña erronea") } }
+        ])
+      }
     }
-    async function inicioSesion(){
-        let respuesta = {estado: true}
-        await signInWithEmailAndPassword(auth, email, password)
-        .then(credencial =>{console.log("Se inició sesion")})
-        .catch(errorr => {
-          respuesta.estado = false         
-        })
-        return respuesta;
-      }
-      const verificarContraseña = async()=>{
-          seterrorContra("")
-         console.log(password)
-               if(!validarContra(password)){
-                          seterrorContra("Contrasña Incorecta")
-               }else {
-                const resultInicioSesion = await inicioSesion()
-                const estaReautenticado = resultInicioSesion.estado
-                 if(estaReautenticado){
-                   if(Tipo == "Eliminar"){
-                    deleteUser(user)
-                    .then(() => {                   
-                    console.log(user)
-                    
-                    navigation.navigate("Login");
-                   
-                      Alert.alert("Cuenta Eliminada ", "La cuenta fue eliminada corrcetamnete",[
-                          {text: "OK" ,onPress: () =>{ console.log("ok a Login")} }
-                          ])
-                    })
-                    .catch(error => { 
-                      console.log(error)
-                        const errorCode = error.code;
-                        const errorMessage = error.message;
-    
-                    }
-                    )
-                   }else{
-                    navigation.navigate("Administrar Cuenta");
-                   }
-                  
-                 }
-               }
-      }
-   return (
+  }
+  return (
     <KeyboardAwareScrollView style={styles.container}>
       <View style={styles.container}>
-     
-       
-         <Image
+
+
+        <Image
           style={styles.incono}
           source={require("../assets/capt.png")}
         />
@@ -102,36 +106,37 @@ const verificarContraseña = ({navigation, route}) =>{
           secureTextEntry={!mostarContra}
           rightIcon={
             <Icon
-                type="material-community"
-                name={ mostarContra ? "eye-outline" : "eye-off-outline"}
-                iconStyle={styles.icon}
-                onPress={() => setmostarContra(!mostarContra)}
+              type="material-community"
+              name={mostarContra ? "eye-outline" : "eye-off-outline"}
+              iconStyle={styles.icon}
+              onPress={() => setmostarContra(!mostarContra)}
             />
-            }
+          }
         />
-        
-        <TouchableOpacity style={styles.botonEliminar}       onPress={() => verificarContraseña()}>
-            <Text style={styles.textEliminar}> {nombreBoton}</Text>
+
+        <TouchableOpacity style={styles.botonEliminar} onPress={() => verificarContraseña()}>
+          <Text style={styles.textEliminar}> {nombreBoton}</Text>
         </TouchableOpacity>
       </View>
     </KeyboardAwareScrollView>
-)}
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
-  flex: 1,
-        backgroundColor: '#001B48',
-        paddingLeft: 15,
-        paddingRight: 15,
-      },
-   
-  incono:{
+    flex: 1,
+    backgroundColor: '#001B48',
+    paddingLeft: 15,
+    paddingRight: 15,
+  },
+
+  incono: {
     resizeMode: 'contain',
     width: "90%",
-    alignSelf:"center",
+    alignSelf: "center",
     maxHeight: "20%"
   },
-  titulo:{
+  titulo: {
     color: "white",
     fontSize: 30,
     marginTop: 15,
@@ -139,31 +144,31 @@ const styles = StyleSheet.create({
     marginBottom: "15%",
     fontWeight: 'bold'
   },
-  text:{
+  text: {
     color: "white",
     fontSize: 20,
   },
-  input:{
+  input: {
     width: "100%",
     borderColor: 'white',
     borderWidth: 4,
     borderRadius: 5,
     marginTop: 5,
     marginBottom: 5,
-    color:"white",
-    backgroundColor:"#001B48"
+    color: "white",
+    backgroundColor: "#001B48"
   },
   icon: {
     color: "white",
     borderColor: "white"
   },
-  recuperarPassword:{
+  recuperarPassword: {
     fontSize: 15,
-    color:"white",
-    textDecorationLine:'underline',
+    color: "white",
+    textDecorationLine: 'underline',
     marginBottom: "18%"
   },
-  botonEliminar:{
+  botonEliminar: {
     backgroundColor: "#0093B7",
     fontSize: 30,
     width: "80%",
@@ -174,16 +179,16 @@ const styles = StyleSheet.create({
     marginTop: 30,
     alignSelf: "center"
   },
-  textEliminar:{
+  textEliminar: {
     color: "white",
     fontSize: 23,
-    
-  },incono:{
+
+  }, incono: {
     resizeMode: 'contain',
-    alignSelf:"center",
+    alignSelf: "center",
     maxHeight: "20%",
     marginTop: 30
-    }
+  }
 });
 
 export default verificarContraseña;
